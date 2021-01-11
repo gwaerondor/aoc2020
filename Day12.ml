59cd94a808dc7e0d1d4379ae8b8b546a2892387f
Robin Larsson <robin.larsson@ericsson.com>
2021-01-11 18:28:57 +0800

    Add solution to day 12 part 2

--- Day12.ml
+++ Day12.ml
@@ -32,17 +32,18 @@ let rec turn ~facing ~deg =
      in
      turn ~facing:next_facing ~deg:next_deg
 
-let rec exec ~x ~y ~facing ~input =
+let rec exec_1 ~ship ~facing ~input =
+  let (x, y) = ship in
   match input with
   | [] -> (abs x) + (abs y)
   | (i :: rest) ->
      match i with
-     | Action (North, n) -> exec ~x:x ~y:(y + n) ~facing:facing ~input:rest
-     | Action (East, n) -> exec ~x:(x + n) ~y:y ~facing:facing ~input:rest
-     | Action (South, n) -> exec ~x:x ~y:(y - n) ~facing:facing ~input:rest
-     | Action (West, n) -> exec ~x:(x - n) ~y:y ~facing:facing ~input:rest
-     | Action (Left, deg) -> exec ~x:x ~y:y ~facing:(turn ~facing:facing ~deg:(360 - deg)) ~input:rest
-     | Action (Right, deg) -> exec ~x:x ~y:y ~facing:(turn ~facing:facing ~deg:deg) ~input:rest
+     | Action (North, n) -> exec_1 ~ship:(x, y + n) ~facing:facing ~input:rest
+     | Action (East, n) -> exec_1 ~ship:(x + n, y) ~facing:facing ~input:rest
+     | Action (South, n) -> exec_1 ~ship:(x, y - n) ~facing:facing ~input:rest
+     | Action (West, n) -> exec_1 ~ship:(x - n, y) ~facing:facing ~input:rest
+     | Action (Left, deg) -> exec_1 ~ship:(x, y) ~facing:(turn ~facing:facing ~deg:(360 - deg)) ~input:rest
+     | Action (Right, deg) -> exec_1 ~ship:(x, y) ~facing:(turn ~facing:facing ~deg:deg) ~input:rest
      | Action (Forward, steps) ->
         let (new_x, new_y) = match facing with
           | North -> (x, y + steps)
@@ -51,8 +52,29 @@ let rec exec ~x ~y ~facing ~input =
           | West -> (x - steps, y)
           | _ -> AocLib.halt "Generic error message."
         in
-        exec ~x:new_x ~y:new_y ~facing:facing ~input:rest
+        exec_1 ~ship:(new_x, new_y) ~facing:facing ~input:rest
+
+let rec transpose ~waypoint:(x, y) ~deg =
+  match deg with
+  | 0 -> (x, y)
+  | _ -> transpose ~waypoint:(y, (-x)) ~deg:(deg - 90)
+
+let rec exec_2 ~ship ~waypoint ~input =
+  let (sx, sy) = ship in
+  let (wx, wy) = waypoint in
+  match input with
+  | [] -> (abs sx) + (abs sy)
+  | (i :: rest) ->
+     match i with
+     | Action (North, n) -> exec_2 ~ship:ship ~waypoint:(wx, wy + n) ~input:rest
+     | Action (East, n) -> exec_2 ~ship:ship ~waypoint:(wx + n, wy) ~input:rest
+     | Action (South, n) -> exec_2 ~ship:ship ~waypoint:(wx, wy - n) ~input:rest
+     | Action (West, n) -> exec_2 ~ship:ship ~waypoint:(wx - n, wy) ~input:rest
+     | Action (Left, deg) -> exec_2 ~ship:ship ~waypoint:(transpose ~waypoint:waypoint ~deg:(360 - deg)) ~input:rest
+     | Action (Right, deg) -> exec_2 ~ship:ship ~waypoint:(transpose ~waypoint:waypoint ~deg:deg) ~input:rest
+     | Action (Forward, steps) -> exec_2 ~ship:(sx + (wx * steps), sy + (wy * steps)) ~waypoint:waypoint ~input:rest
 
 let main =
-  let s1 = exec ~x:0 ~y:0 ~facing:East ~input:input in
-  AocLib.print_two_part_solution (s1 |> Int.to_string) "?"
+  let s1 = exec_1 ~ship:(0, 0) ~facing:East ~input:input in
+  let s2 = exec_2 ~ship:(0, 0) ~waypoint:(10, 1) ~input:input in
+  AocLib.print_two_part_solution (s1 |> Int.to_string) (s2 |> Int.to_string)
